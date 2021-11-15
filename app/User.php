fc6a2de93cb7261750686cefce88f7c03563e8e5
Nikola <16414020+nvasic88@users.noreply.github.com>
2021-11-15 23:30:12 +0100

    Load notifications async and paginated Allon non-verified user to access some additional endpoints

--- app/User.php
+++ app/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasLocalePreferen
      *
      * @var array
      */
-    protected $appends = ['full_name'];
+    protected $appends = ['full_name', 'is_verified'];
 
     /**
      * The channels the user receives notification broadcasts on.
@@ -156,6 +156,16 @@ class User extends Authenticatable implements MustVerifyEmail, HasLocalePreferen
     public function getFullNameAttribute()
     {
         return $this->first_name.' '.$this->last_name;
+    }
+
+    /**
+     * Check if user has verified their email address.
+     *
+     * @return bool
+     */
+    public function getIsVerifiedAttribute()
+    {
+        return ! is_null($this->email_verified_at);
     }
 
     /**
