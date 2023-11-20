02e95236044393a65f7c2a4eeb528b054c50cc8d
Nikola Vasić <16414020+nvasic88@users.noreply.github.com>
2023-11-20 18:52:07 +0100

    XFrame redesign

--- app/Http/Middleware/XFrameHeadersMiddleware.php
+++ app/Http/Middleware/XFrameHeadersMiddleware.php
@@ -6,30 +6,13 @@ use Closure;
 
 class XFrameHeadersMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     *
-     * @return mixed
-     */
+
     public function handle($request, Closure $next)
     {
         $response = $next($request);
 
-        $xFrameOptions = 'SAMEORIGIN';
+        $response->headers->set('X-Frame-Options', env('X_FRAME_OPTIONS'));
 
-        // In this example, we are only allowing the third party to include the "iframe" route
-        // It's always better to scope this to a given route / set of routes to avoid any unattended security problems
-        if ($request->routeIs('iframe') && $xFrameOptions = env('X_FRAME_OPTIONS', 'SAMEORIGIN')) {
-            if (false !== strpos($xFrameOptions, 'ALLOW-FROM')) {
-                $url = trim(str_replace('ALLOW-FROM', '', $xFrameOptions));
-
-                return $response->header('Content-Security-Policy', 'frame-ancestors '.$url);
-            }
-        }
-
-        return $response->header('X-Frame-Options', $xFrameOptions);
+        return $response;
     }
 }
